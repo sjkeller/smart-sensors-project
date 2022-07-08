2080fafce09aacb2c4f50b7b1c9cdc317eb6b584
Marek <marek.jahnke@tuhh.de>
2022-07-08 13:17:13 +0200

    fix plotting correct values

--- PBL5/plotter.py
+++ PBL5/plotter.py
@@ -1,21 +1,26 @@
 import serial
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import struct
 
 out = 0
-data = []
+data = [0]
 monitor = serial.Serial(port="/dev/tty.usbserial-A50285BI", baudrate=9600)
 
 fig = plt.figure()
 axis = fig.add_subplot(1,1,1)
 
 def animate(i):
-    raw = monitor.readline()
-    out = int.from_bytes(raw, "little", signed=True)
-    print(out)
-    data.append(out - 675000)
-    axis.clear()
-    axis.plot(data[-50:])
+    r = ord(monitor.read())
+    if(r== 10):
+        raw = monitor.read(2)
+        nums = struct.unpack('>1h', raw)
+        #print(nums)
+        val = nums[0]
+        print(val)
+        data.append(val)
+        axis.clear()
+        axis.plot(data[-50:])
 
 
 anim = animation.FuncAnimation(fig, animate, interval=1)
